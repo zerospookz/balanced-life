db5accb05c825039875a6af8197001f559613902
zerospookz <viktor.v.velev@gmail.com>
2026-01-19 10:19:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeSync v7.8 SAFE SERVICE WORKER (no addAll)
-const CACHE_NAME = "lifesync-v7.8.0";
+// LifeSync v7.9 SAFE SERVICE WORKER (no addAll)
+const CACHE_NAME = "lifesync-v7.9.0";
 self.addEventListener("install", event => {
   self.skipWaiting();
 });
